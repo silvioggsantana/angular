fbc4fd87a18aece9c63bc48c669d66c0fe27fcbf
Silvio Gabriel <silvioggsantana@gmail.com>
2025-08-13 07:52:32 -0300

    adicionando exercícios feitos em aula

---
+++ faculdade/exercicio1.js
@@ -0,0 +1,7 @@
+var n1 = 2 ;
+
+var n2 = 5 ;
+
+var soma = n1 + n2; 
+
+console.log(soma);
---
+++ faculdade/exercicio2.ts
@@ -0,0 +1,14 @@
+var nome:string = "Nelson";
+var idade:number = 20;
+var salario:number = 1234.56;
+
+var numHex:number = 0xf00d ;
+var numOctal:number = 0o744;
+var numBinario:number = 0b1010;
+
+console.log("Nome :"+ nome) ;
+console.log("Idade :"+ idade) ;
+console.log("Salário :"+ salario) ;
+console.log("numHex :"+ numHex) ;
+console.log("numOctal :"+ numOctal) ;
+console.log("numBinario :"+ numBinario) ;
